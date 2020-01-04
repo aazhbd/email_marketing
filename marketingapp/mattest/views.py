69eb85189da1e000262dd4a9ed017c6975cf2e8d
strumy <syedatasneemrumy@gmail.com>
2020-01-04 02:47:53 +0100

    Added functionality to redirect root page to login page.

--- marketingapp/mattest/views.py
+++ marketingapp/mattest/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from mattest.models import Campaign
 
 def homeView(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/user/")
+
     campaigns = Campaign.objects.filter(status=True)
     return render(request, 'home.html', {'campaigns': campaigns})
