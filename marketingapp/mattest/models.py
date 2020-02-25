e064d9cdc9d4e8e6eb9779a6c88c226035b09e1c
AAZ H <aazhbd@yahoo.com>
2020-02-25 13:09:07 +0100

    Updated datetime field and material UI improvements.

--- marketingapp/mattest/models.py
+++ marketingapp/mattest/models.py
@@ -33,7 +33,7 @@ class ContactList(Base):
 
 class Campaign(Base):
     name = models.CharField(max_length=200, blank=False)
-    start_date = models.DateField(blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
     description = models.CharField(max_length=400, blank=True)
     email_subject = models.CharField(max_length=200, blank=True)
     email_body = models.TextField(blank=True)
